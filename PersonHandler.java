24809750c20f367441265e5190248d4756fd7d76
Cristina McClintock <cristinamcclintock@zipcoders-MacBook-Pro.local>
2019-02-08 21:37:41 -0500

    Completed PersonHandlerTest

--- PersonHandler.java
+++ PersonHandler.java
@@ -1,4 +1,4 @@
-package com.zipcodewilmington;
+ 
 
 /**
  * Created by leon on 1/24/18.
@@ -21,6 +21,16 @@ public class PersonHandler {
                 // append `stringRepresentation` to `result` variable
 
             // end loop
+            int counter = 0;
+            while (counter < personArray.length) {
+                String somePerson = personArray[counter].toString();
+                result += somePerson;
+                
+                System.out.println(personArray[counter]);
+                counter++;
+            }
+            
+            
         return result;
     }
 
@@ -38,6 +48,12 @@ public class PersonHandler {
                 // get `string Representation` of `currentPerson`
                 // append `stringRepresentation` to `result` variable
             // end loop
+            
+           for (int i = 0; i < personArray.length; i++){
+               String myString = personArray[i].toString();
+               result += myString;
+               
+            }
 
         return result;
     }
@@ -48,12 +64,20 @@ public class PersonHandler {
         String result = "";
         // identify array's type
         // identify array's variable-name
-
+        
+       
         // use the above discoveries to declare for-each-loop signature
             // begin loop
                 // get `string Representation` of `currentPerson`
                 // append `stringRepresentation` to `result` variable
             // end loop
+            for (Person pers: personArray) {
+                String somePer = pers.toString();
+                
+                result += pers.toString();
+                
+               
+            }
 
         return result;
     }
